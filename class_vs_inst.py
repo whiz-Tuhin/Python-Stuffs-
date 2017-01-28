4fdf5496ce05164972508815a3ddddb57fce321a
Tuhin Khare <tuhinkhare3@gmail.com>
2017-01-28 15:35:30 +0530

    Some stuff i wanna see

---
+++ class_vs_inst.py
@@ -0,0 +1,10 @@
+class female():
+    gender = "female"
+
+    def get_name(self,name):
+        print "Name " + name + " Gender " + self.gender
+
+
+ob1 = female()
+ob1.get_name("phoebe")
+ob1.get_name("monica")
